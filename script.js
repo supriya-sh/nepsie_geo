fcdeec2e09c29da2384d4ff461d52e09c1c07b81
supriya-sh <shz.supriya@gmail.com>
2026-09-22 22:24:04 +0545

    map added

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 function toggleMenu() {
     document.getElementById("opt").classList.toggle("show");
 }
+//leaflet standard documentation: not coding 
+const nepalBounds = [
+    [26.347, 80.058], // Southwest boundary
+    [30.447, 88.201]  // Northeast boundary
+];
+const map = L.map('map', {
+    maxBounds: nepalBounds,
+    maxBoundsViscosity: 1.0, // Prevents panning outside Nepal
+    minZoom: 7,
+    maxZoom: 15
+}).setView([28.3949, 84.1240], 6.5); // Centered on Nepal at zoom level 7
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '© OpenStreetMap contributors'
+}).addTo(map);
